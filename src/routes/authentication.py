e94dde8a0dcf8727ba63e930d638fc77f74b9b28
slaughtbear <ivanaguirre368@gmail.com>
2025-10-26 16:56:25 -0600

    refactor(authentication): documentar y refactorizar endpoint de registro de usuarios

--- src/routes/authentication.py
+++ src/routes/authentication.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from src.models.users import User, UserCreate
+from fastapi.responses import JSONResponse
+
+from src.models.users import User, UserSignUp
+from src.models.tokens import Token
+from src.schemas.users import user_schema
+
 from src.authentication.jwt_auth import authenticate_user, create_access_token
 from src.authentication.security import hash_password
+
 from src.database.queries.users import add_user
-from src.schemas.users import user_schema
-from src.models.tokens import Token
+
 
 router = APIRouter()
+
 
 @router.post("/signin", response_model=Token)
 async def signin(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -26,8 +32,30 @@ async def signin(form_data: OAuth2PasswordRequestForm = Depends()):
         "token_type": "bearer"
     }
 
-@router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=User)
-async def signup(user_data: UserCreate) -> User:
+
+@router.post("/signup", response_model=User)
+async def signup(user_data: UserSignUp) -> JSONResponse:
+    """Endpoint para registrar a un usuario.
+    Args:
+        user_data (UserSignUp): Datos necesarios para el registro.
+    Returns:
+        JSONResponse: Respuesta JSON personalizada.
+    """
+    # Hashing de contraseña
     user_data.password = hash_password(user_data.password)
-    response = await add_user(user_data.model_dump())
-    return user_schema(response)
+
+    # Datos por defecto
+    user_dict = user_data.model_dump() # copia de datos en diccionario
+    user_dict["role"] = "commenter" # rol de usuario
+    user_dict["disabled"] = False # cuenta activa
+
+    # Operación de creación del usuario en la base de datos
+    response = await add_user(user_dict)
+
+    return JSONResponse(
+        content={
+            "status": "success",
+            "message": f"{response["username"]} registrado correctamente"
+        },
+        status_code=status.HTTP_201_CREATED
+    )
