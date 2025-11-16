87ce25fe2b8346e95f9a4afcce34e14fc38b4dfc
slaughtbear <ivanaguirre368@gmail.com>
2025-11-16 11:02:03 -0600

    chore: actualizar caracteres mínimos para username

--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
 
+
 class User(BaseModel):
     id: str
     username: str
@@ -8,25 +9,28 @@ class User(BaseModel):
     role: str
     disabled: bool
 
+
 class UserInDB(User):
     password: str
 
+
 class UserCreate(BaseModel):
-    username: str = Field(..., min_length=6, max_length=15)
+    username: str = Field(..., min_length=5, max_length=15)
     full_name: str = Field(..., min_length=6, max_length=128)
     email: EmailStr = Field(..., min_length=6, max_length=254)
-    role: str = Field(...)
+    role: str | None = None
     password: str = Field(...)
-    disabled: bool = False
+
 
 class UserSignUp(BaseModel):
-    username: str = Field(..., min_length=6, max_length=15)
+    username: str = Field(..., min_length=5, max_length=15)
     full_name: str = Field(..., min_length=6, max_length=128)
     email: EmailStr = Field(..., min_length=6, max_length=254)
     password: str = Field(...)
 
+
 class UserUpdate(BaseModel):
-    username: str | None = Field(None, min_length=6, max_length=15)
+    username: str | None = Field(None, min_length=5, max_length=15)
     full_name: str | None = Field(None, min_length=6, max_length=128)
     email: EmailStr | None = Field(None, min_length=6, max_length=254)
     role: str | None = Field(None, min_length=6, max_length=32)
