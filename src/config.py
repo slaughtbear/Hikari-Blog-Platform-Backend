96cb68f78f3e0e8344773ea98cc0892fb1a9bdb5
slaughtbear <ivanaguirre368@gmail.com>
2025-11-16 10:59:49 -0600

    chore: agregar variable de entorno de MongoDB Atlas

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str
+    MONGO_ATLAS: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: str
